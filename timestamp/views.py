c8668bb9b48b33d2ca015d719dbd5118ac37c694
alchermd <johnalcherdoloiras@gmail.com>
2017-12-21 21:05:39 +0800

    Add minimal comments.

--- timestamp/views.py
+++ timestamp/views.py
@@ -14,6 +14,7 @@ def index():
 @app.route('/<int:timestamp_unix>')
 def get_unix_timestamp(timestamp_unix):
     try:
+        # Zero or negative timestamps are invalid.
         if timestamp_unix <= 0:
             raise ValueError
 
@@ -29,11 +30,18 @@ def get_unix_timestamp(timestamp_unix):
 @app.route('/<timestamp_natural>')
 def get_natural_timestamp(timestamp_natural):
     try:
+        # Flask handles query strings starting with a '-' as strings,
+        # so if it converts to int successfully (i.e a negativ integer),
+        # let the get_unix_timestamp route handle it.
         unix = int(timestamp_natural)
         return get_unix_timestamp(unix)
+
     except ValueError:
         try:
+            # Parse 'June 05, 1996' into a datetime, and then format
+            # it to remove the time part.
             natural = parser.parse(timestamp_natural).strftime('%B %d, %Y')
+
         except ValueError:
             return jsonify({'natural': None, 'unix': None})
 
